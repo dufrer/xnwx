2a571927502a9afe9e4e23174ba7f0109f78b8d2
dufrer <nian-qing88@qq.com>
2013-11-29 16:31:03 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xnwx::Application.config.secret_token = '385ba9b37e54680cea36bb15571e3e6c5037ae99c5a6744a3213a2904f9a60df8dc1d9fd3bd2f5eacef9bf3a030b147fc97fabd8c7fb7e98e04262f1702910b9'
